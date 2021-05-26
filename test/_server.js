5df7283f54d829b55b39ab98d2b5a51fe6dba625
Antony <antony.chiossi@yoroi.company>
2021-05-26 15:34:48 +0200

    fix test

--- test/_server.js
+++ test/_server.js
@@ -1,25 +1,28 @@
-import http from 'http';
-import handler from 'serve-handler';
+import http from "http";
+import handler from "serve-handler";
 
-export function createStaticServer (pathRoot) {
+export function createStaticServer(pathRoot) {
   const opts = {
-    public: pathRoot
+    public: pathRoot,
   };
 
-  const server = http.createServer((request, response) => handler(request, response, opts));
+  const server = http.createServer((request, response) =>
+    handler(request, response, opts)
+  );
 
-  server.stop = () => new Promise((resolve, reject) => {
-    server.close(error => {
-      error ? reject(error) : resolve();
+  server.stop = () =>
+    new Promise((resolve, reject) => {
+      server.close((error) => {
+        error ? reject(error) : resolve();
+      });
     });
-  });
 
   return new Promise((resolve, reject) => {
-    server.on('error', reject);
-    server.listen(0, '0.0.0.0', () => {
+    server.on("error", reject);
+    server.listen(6963, "0.0.0.0", () => {
       const { address, port } = server.address();
       server.url = `http://${address}:${port}`;
       resolve(server);
     });
   });
-};
+}
